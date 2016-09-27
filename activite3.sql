be20d53fd33db5ebddd6063f28472bdeb4537102
nicolaspschd <nicolas.pschd@eduge.ch>
2016-09-27 13:04:40 +0200

    Add files via upload

---
+++ activite3.sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win32 (AMD64)
+--
+-- Host: 127.0.0.1    Database: activite3
+-- ------------------------------------------------------
+-- Server version	5.6.15-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `personne`
+--
+
+DROP TABLE IF EXISTS `personne`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personne` (
+  `idpersonne` int(11) NOT NULL AUTO_INCREMENT,
+  `nomPersonne` varchar(45) DEFAULT NULL,
+  `idVehicule` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idpersonne`),
+  UNIQUE KEY `idpersonne_UNIQUE` (`idpersonne`),
+  KEY `fk_idVehicule_idx` (`idVehicule`),
+  CONSTRAINT `fk_idVehicule` FOREIGN KEY (`idVehicule`) REFERENCES `vehicules` (`idvehicules`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personne`
+--
+
+LOCK TABLES `personne` WRITE;
+/*!40000 ALTER TABLE `personne` DISABLE KEYS */;
+INSERT INTO `personne` VALUES (1,'Oui',2),(2,'Raphi Raphou',1),(3,'Micha',3),(4,'Marco',5),(5,'Albert',4),(6,'Robert',6),(7,'Uelli',2);
+/*!40000 ALTER TABLE `personne` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicules`
+--
+
+DROP TABLE IF EXISTS `vehicules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vehicules` (
+  `idvehicules` int(11) NOT NULL AUTO_INCREMENT,
+  `nomVehicule` varchar(45) DEFAULT NULL,
+  UNIQUE KEY `idvehicules_UNIQUE` (`idvehicules`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicules`
+--
+
+LOCK TABLES `vehicules` WRITE;
+/*!40000 ALTER TABLE `vehicules` DISABLE KEYS */;
+INSERT INTO `vehicules` VALUES (1,'Audi'),(2,'Tesla'),(3,'VolksWagen'),(4,'Mercedes'),(5,'Skoda'),(6,'Volvo');
+/*!40000 ALTER TABLE `vehicules` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-09-20 16:12:24
